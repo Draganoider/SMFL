c92c7ecb543094d05ec67d92528cc6bc4cc89391
Євген Міщенко <Yevhen.Mishchenko@cs.khpi.edu.ua>
2022-03-08 08:56:12 +0200

    massive of balls flying

--- SMFL/Main.cpp
+++ SMFL/Main.cpp
@@ -1,27 +1,71 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <cstdlib>
 #include "windows.h"
+
+int trueRandom(int max)
+{
+    return rand() * max / 32767;
+}
 
 int main()
 {
+    const int N = 20;
+    const int D = 50;
 
-    int resolutionx = 1500;
+
+    int initX[N];
+    int initY[N];
+    
+    bool p = false;
+    int resolutionx = 1200;
     int resolutiony = 1500;
     // create the window
     sf::RenderWindow window(sf::VideoMode(resolutionx, resolutiony), "My window");
     
-    sf::CircleShape shape (50.f);
-    shape.setFillColor(sf::Color(150, 50, 250));
-    int trueresolutionx = resolutionx - 100;
+    
+    int trueresolutionx = resolutionx - D*2;
+    int trueresolutiony = resolutiony - D*2;
 
-    int trueresolutiony = resolutiony - 100;
-    int i = 0;
-    int curentx = 0;
-    int curenty = 0;
-    int newx = 0;
-    int newy = 0;
-    int dx = 10;
-    int dy = 3;
+    sf::CircleShape shape[N];
+    int curentx[N];
+    int curenty[N];
+    int newx[N];
+    int newy[N];
+    int dx[N];
+    int dy[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        shape[i] = sf::CircleShape(D);
+        shape[i].setFillColor(sf::Color(150, 50, 250));
+
+        curentx[i] = 0;
+        curenty[i] = 0;
+        newx[i] = 0;
+        newy[i] = 0;
+        dx[i] = trueRandom(5) + 1;
+        dy[i] = trueRandom(5) + 1;
+
+
+        initX[i] = trueRandom(resolutionx - D*2) + D;
+        initY[i] = trueRandom(resolutiony - D*2) + D;
+
+
+    }
+
+    
+
+
+  /*  sf::CircleShape shape1(100.f);
+    shape1.setFillColor(sf::Color::Black);*/
+
+
+
+
+
+
+
 
 
     // run the program as long as the window is open
@@ -39,37 +83,58 @@ int main()
         // clear the window with black color
         window.clear(sf::Color::Cyan);
        
-        
-        window.draw(shape);
-        //shape.setPosition(243.f, 368.f);
-        Sleep(2);
-        newx = curentx + dx;
-        curentx = newx;
-        newy = curenty + dy;
-        curenty = newy;
-        if (newx >= trueresolutionx)
+        if (!p)
         {
-            dx = -dx;
-        }
-      
-        if (newy >= trueresolutiony)
-        {
-            dy = -dy;
-        }
-        if (newx <= 0)
-        {
-            dx = -dx;
+            for (int i = 0; i < N; i++)
+            {
+                curentx[i] = initX[i];
+                curenty[i] = initY[i];
+
+                shape[i].setPosition(initX[i], initY[i]);
+                
+            }
+            p = true;
         }
 
-        if (newy <= 0)
-        {
-            dy = -dy;
+        for (int i = 0; i < N; i++) {
+
+            window.draw(shape[i]);
+
+           
+
+           
+
+
+            //shape.setPosition(243.f, 368.f);
+            //Sleep(2);
+
+            newx[i] = curentx[i] + dx[i];
+            curentx[i] = newx[i];
+            newy[i] = curenty[i] + dy[i];
+            curenty[i] = newy[i];
+
+            if (newx[i] >= trueresolutionx)
+            {
+                dx[i] = -dx[i];
+            }
+
+            if (newy[i] >= trueresolutiony)
+            {
+                dy[i] = -dy[i];
+            }
+
+            if (newx[i] <= 0)
+            {
+                dx[i] = -dx[i];
+            }
+
+            if (newy[i] <= 0)
+            {
+                dy[i] = -dy[i];
+            }
+
+            shape[i].setPosition(newx[i], newy[i]);
         }
-
-        shape.setPosition(newx, newy);
-
-
-
 
 
         
@@ -83,3 +148,4 @@ int main()
 
     return 0;
 }
+
